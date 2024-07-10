0550e748a3382a799f39e6b01093289009a93d9e
SAHIL13928 <148043150+SAHIL13928@users.noreply.github.com>
2024-07-10 23:08:20 +0530

    Initial commit

---
+++ src/components/LineChart/LineChart.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import Chart from 'react-google-charts';
+
+const LineChart = ({ historicalData }) => {
+  const [data, setData] = useState([["Date", "Prices"]]);
+
+  useEffect(() => {
+    let dataCopy = [["Date", "Prices"]];
+    if (historicalData.prices) {
+      historicalData.prices.forEach((item) => {
+        dataCopy.push([new Date(item[0]), item[1]]);
+      });
+      setData(dataCopy);
+    }
+  }, [historicalData]);
+
+  return (
+    <Chart
+      chartType="LineChart"
+      data={data}
+      options={{
+        hAxis: {
+          title: 'Date',
+          format: 'MMM dd, yyyy'
+        },
+        vAxis: {
+          title: 'Prices'
+        },
+        legend: { position: 'bottom' }
+      }}
+      height="400px"
+    />
+  );
+};
+
+export default LineChart;
